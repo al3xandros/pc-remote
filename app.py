836c9c06e8858c0830e8313a4705c0d758157e3d
al3xandros <56219114+al3xandros@users.noreply.github.com>
2022-06-16 10:28:06 +0300

    Add files via upload
    Added main project

---
+++ app.py
@@ -0,0 +1,64 @@
+import external
+from flask import *
+from flask_socketio import *
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = external.SECRET_KEY
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return """
+<a href="keyboard">keyboard</a>
+<a href="mouse">mouse</a>
+"""
+
+# --------------------------------- keyboard ---------------------------------
+@app.route("/keyboard/")
+def keyboard():
+    return render_template("keyboard2.html")
+
+
+@socketio.on('keystroke')
+def keystroke(data):
+    if external.VERBOSE:
+        print("-"*36)
+        print(data)
+        print("-"*36)
+    external.handle_keystroke(data)
+
+
+
+# --------------------------------- mouse ---------------------------------
+@app.route("/mouse/")
+def mouse():
+    return render_template("mouse2.html")
+
+
+@socketio.on('mousemove')
+def keystroke(data):
+    if external.VERBOSE:
+        print("-"*36)
+        print(data)
+        print("-"*36)
+    external.handle_mousemove(data)
+
+
+@socketio.on('mouseclick')
+def keystroke(data):
+    if external.VERBOSE:
+        print("-"*36)
+        print(data)
+        print("-"*36)
+    external.handle_mouseclick(data)
+
+
+if __name__ == '__main__':
+    if external.VERBOSE:
+        print("pc-remote started.")
+    socketio.run(app,
+                 host=external.HOST,
+                 port=external.PORT,
+                 debug=external.DEBUG)
---
+++ external.py
@@ -0,0 +1,55 @@
+import pyautogui as pg # pygui
+import json
+
+
+HOST = "0.0.0.0"
+PORT = 5000
+DEBUG  = False
+VERBOSE = False
+SECRET_KEY = "Change me to something random!"
+MOUSE_SPEED = 10
+
+
+def handle_keystroke(data):
+    d = json.loads(data)
+
+    modifiers = [
+        "capslock",
+        "shift",
+        "ctrl",
+        "fn",
+        "win",
+        "alt"
+    ]
+    for k, v in d.items():
+        if v and k in modifiers:
+            pg.keyDown(k)
+
+    pg.press(d["key"])
+
+    for k, v in d.items():
+        if v and k in modifiers:
+            pg.keyUp(k)
+
+
+# def handle_mousemove(data):
+#     d = json.loads(data)
+#     pg.moveRel(d['x'], d['y'], duration=0.2)
+
+
+def handle_mousemove(data):
+    d = json.loads(data)
+    if d['is_touch']:
+        x, y = pg.position()
+        x += d["x"] // MOUSE_SPEED
+        y += d["y"] // MOUSE_SPEED
+        pg.moveTo(x, y)
+    else:
+        pg.moveRel(d['x'], d['y'], duration=0.2)
+
+
+
+
+def handle_mouseclick(data):
+    d = json.loads(data)
+    pg.click(button=d.get('key'))
